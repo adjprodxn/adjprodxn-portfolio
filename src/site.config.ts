d5a18fcdd127e68f6de293587cd8e8bd331138d6
adjprodxn <amandeepedts@gmail.com>
2025-12-07 13:40:57 -0800

    feat(seo): implement on-page and schema optimizations

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,7 +14,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "Portfolio for an IT Technician & Ethical Hacker",
+	description: "Portfolio for aDJ Prodxn, an IT Technician & Ethical Hacker specializing in network security, system administration, and digital forensics.",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
@@ -25,7 +25,7 @@ export const siteConfig: SiteConfig = {
 		- The link value found in src/components/layout/Header.astro L:35
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
-	title: "aDJ Prodxn Portfolio",
+	title: "aDJ Prodxn | Network Security & IT Services",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
 	url: "https://adjprodxn.ca/",
 };
